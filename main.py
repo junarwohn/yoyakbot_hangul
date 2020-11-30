56d35b787d7a818a80ecf3eed49df038a03f53a9
Joonho Wohn <junarwohn@hanyang.ac.kr>
2020-11-30 19:34:39 +0900

    update

--- main.py
+++ main.py
@@ -4,16 +4,23 @@ import os
 import re
 import numpy as np
 import difflib
+from difflib import SequenceMatcher
 
 
 def img_similarity(img1, img2):
+    
+    #gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    #gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    #data1 = gray1.flatten()
+    #data2 = gray2.flatten()
     data1 = img1.flatten()
     data2 = img2.flatten()
-    cnt = 0
-    for p1, p2 in zip(data1, data2):
-        if p1 == p2:
-            cnt += 1
-    return cnt / len(data1)
+    return sum(np.isclose(data1, data2, atol=50)) / len(data1)
+    #cnt = 0
+    #for p1, p2 in zip(data1, data2):
+    #    if p1 == p2:
+    #        cnt += 1
+    #return cnt / len(data1)
 
 
 file_list = os.listdir("src/extract/")
@@ -30,6 +37,9 @@ kernel_sharpen = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
 add_cnt = 0
 page_cnt = 0
 thumb_cnt = 0
+str_diff = 0
+img_diff = 0
+ok = 13
 # selection
 print("load preset? [{}:{}] [y/n]".format(str(height_upper), str(height_lower)))
 yn = input()
@@ -67,7 +77,7 @@ cur_img = cv2.imread("src/extract/" + file_list[0])[height_upper:height_lower, :
 gray = cv2.cvtColor(cur_img, cv2.COLOR_BGR2GRAY)
 inverted = cv2.bitwise_not(gray)
 bilateral_filter = cv2.bilateralFilter(inverted, 9, 16, 16)
-r, pre_bin = cv2.threshold(bilateral_filter, 127, 255, cv2.THRESH_BINARY)
+r, pre_bin = cv2.threshold(bilateral_filter, 200, 255, cv2.THRESH_BINARY)
 for file_name in file_list:
     original_img = cv2.imread("src/extract/" + file_name)
     # cv2.imshow("original_img", original_img)
@@ -88,77 +98,78 @@ for file_name in file_list:
     # dst = cv2.filter2D(inverted, -1, kernel_sharpen)
     # new_img = dst
     text = image_to_string(dst, lang="Hangul", config="--psm 4 --oem 1")
-    # text = image_to_string(dst, lang="kor", config="--psm 4 --oem 1")
-    word_list = re.sub("\d+|[ ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎ\{\}\[\]\/?.,;:|\)「＊ㆍ：”…*~`!^\-_+<>@\#$%&\\\=\(\'\"]", "", text).split('\n')
-    # word_list = re.sub("\d+|[ \{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]", "", text).split('\n')
+    word_list = re.sub("\d+|[ ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎㅏㅕㅓㅕㅗㅛㅜㅠㅡㅣ\{\}\[\]\/?.,;:|\)「＊ㆍ：”…*~`!^\-_+<>@\#$%&\\\=\(\'\"\f]|[A-Za-z]", "", text).split('\n')
     cur_word = max(word_list, key=len)
-    img_diff = img_similarity(pre_bin, cur_bin)
+    # Filter trash recognition
+    if len(cur_word) < 2:
+        continue
     if cur_word != pre_word:
-        print(img_diff)
-        diff_len = abs(len(list(diff.compare(cur_word, pre_word))) - min(len(cur_word), len(pre_word)))
-        if diff_len > len(cur_word) / 4:
-            print(cur_word)
-            ok = 13
-            # if diff_len < 10 or abs(len(cur_word) - len(pre_word)) < 4 or img_diff < 0.05:
+        str_diff= SequenceMatcher(None, cur_word, pre_word).ratio()
+        #img_diff = img_similarity(pre_bin, cur_bin)
+        if str_diff > 0.9:
+            #print("str pass : {:.03f}, img diff : {:.03f}, pre : [{}], cur : [{}]".format(str_diff, img_diff, pre_word, cur_word))
+            continue
+        elif str_diff > 0.2:
+            img_diff = img_similarity(pre_img, cur_img)
+            # same word. obviously.
             if img_diff > 0.98:
+                #print("str diff: {:.03f}, img pass : {:.03f}, pre : [{}], cur : [{}]".format(str_diff, img_diff, pre_word, cur_word))
                 continue
-            if img_diff > 0.9:
-                print("Check something")
+            if img_diff > 0.85:
+                print("Check something [{}], [{}]".format(pre_word, cur_word) )
                 cv2.imshow("dst", dst)
                 cv2.imshow("cur_bin", cur_bin)
                 add_img = cv2.addWeighted(pre_img, 0.5, cur_img, 0.5, 0)
                 cv2.imshow("Okay to enter", add_img)
                 ok = cv2.waitKey(0)
                 cv2.destroyAllWindows()
-                # ok = input()
-            if ok == 13 and len(cur_word) > 3:
-                """ MULTILINE CHECK """
-                cur_img2 = original_img[2 * height_upper - height_lower:height_upper, :]
-                gray2 = cv2.cvtColor(cur_img2, cv2.COLOR_BGR2GRAY)
-                inverted2 = cv2.bitwise_not(gray2)
-                bilateral_filter2 = cv2.bilateralFilter(inverted2, 9, 16, 16)
-                r, cur_bin2 = cv2.threshold(bilateral_filter2, 127, 255, cv2.THRESH_BINARY)
-                # r, cur_bin2 = cv2.threshold(inverted2, 127, 255, cv2.THRESH_BINARY)
-                # cur_bin2 = cv2.bilateralFilter(cur_bin2, 9, 16, 16)
-                #
-                # dst2 = cur_bin2
-                dst2 = bilateral_filter2
-                text = image_to_string(dst2, lang="Hangul", config="--psm 4 --oem 1")
-                # text = image_to_string(dst2, lang="kor", config="--psm 4 --oem 1")
-                word_list = re.sub("\d+|[ ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎ\{\}\[\]\/?.,;:|\)「＊ㆍ：”…*~`!^\-_+<>@\#$%&\\\=\(\'\"]", "",
-                                   text).split('\n')
-                # word_list = re.sub("\d+|[ \{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]", "", text).split('\n')
-                cur_word2 = max(word_list, key=len)
-                """ MULTILINE CHECK """
+            if ok != 13:
+                continue
 
-                if len(cur_word2) > len(cur_word):
-                    print("Check multiline")
-                    cv2.imshow("cur_img", cur_img)
-                    cv2.imshow("cur_img2", cur_img2)
-                    ok = cv2.waitKey(0)
-                    cv2.destroyAllWindows()
-                    if ok == 13:
-                        result_img = np.vstack((result_img, cur_img2))
-                    else:
-                        result_img = np.vstack((result_img, cur_img))
-                else:
-                    result_img = np.vstack((result_img, cur_img))
+        if (0.9 > str_diff > 0.25):
+        #if (True):
+            print("str diff : {:.03f}, img diff : {:.03f}, pre : [{}], cur : [{}]".format(str_diff, img_diff, pre_word, cur_word))
+        else:
+            print("str diff : {:.03f}, img diff : -.---, pre : [{}], cur : [{}]".format(str_diff, pre_word, cur_word))
+        cur_img2 = original_img[2 * height_upper - height_lower:height_upper, :]
+        gray2 = cv2.cvtColor(cur_img2, cv2.COLOR_BGR2GRAY)
+        inverted2 = cv2.bitwise_not(gray2)
+        bilateral_filter2 = cv2.bilateralFilter(inverted2, 9, 16, 16)
+        r, cur_bin2 = cv2.threshold(bilateral_filter2, 127, 255, cv2.THRESH_BINARY)
+        dst2 = bilateral_filter2
+        text = image_to_string(dst2, lang="Hangul", config="--psm 4 --oem 1")
+        word_list = re.sub("\d+|[ ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎㅏㅑㅓㅕㅗㅛㅜㅠㅡㅣ\{\}\[\]\/?.,;:|\)「＊ㆍ：”…*~`!^\-_+<>@\#$%&\\\=\(\'\"\f]|[A-Za-z]", "",
+                           text).split('\n')
+        cur_word2 = max(word_list, key=len)
+        """ MULTILINE CHECK """
+        if len(cur_word2) > len(cur_word):
+            print("Check multiline")
+            cv2.imshow("cur_img", cur_img)
+            cv2.imshow("cur_img2", cur_img2)
+            ok = cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            if ok == 13:
+                result_img = np.vstack((result_img, cur_img2))
+            else:
+                result_img = np.vstack((result_img, cur_img))
+        else:
+            result_img = np.vstack((result_img, cur_img))
 
-                add_cnt += 1
-                if add_cnt % 25 == 0:
-                    cv2.imwrite("result-{}.jpg".format(str(page_cnt)), result_img)
-                    page_cnt += 1
-                    thumb_cnt += 1
-                    result_img = cv2.imread("src/thumbs/" + thumb_list[thumb_cnt])[:height_upper - 5, :]
-                #
-                # if add_cnt % 40 == 0:
-                #     cv2.imwrite("result-{}.jpg".format(str(page_cnt)), result_img)
-                #     page_cnt += 1
-                #     thumb_cnt += 1
-                #     result_img = cv2.imread("src/thumbs/" + thumb_list[thumb_cnt])[:height_upper - 5, :]
-                # elif add_cnt % 20 == 0:
-                #     thumb_cnt += 1
-                #     result_img = np.vstack((result_img, cv2.imread("src/thumbs/" + thumb_list[thumb_cnt])[:height_upper - 5, :]))
+        add_cnt += 1
+        if add_cnt % 25 == 0:
+            cv2.imwrite("result-{}.jpg".format(str(page_cnt)), result_img)
+            page_cnt += 1
+            thumb_cnt += 1
+            result_img = cv2.imread("src/thumbs/" + thumb_list[thumb_cnt])[:height_upper - 5, :]
+            #
+            # if add_cnt % 40 == 0:
+            #     cv2.imwrite("result-{}.jpg".format(str(page_cnt)), result_img)
+            #     page_cnt += 1
+            #     thumb_cnt += 1
+            #     result_img = cv2.imread("src/thumbs/" + thumb_list[thumb_cnt])[:height_upper - 5, :]
+            # elif add_cnt % 20 == 0:
+            #     thumb_cnt += 1
+            #     result_img = np.vstack((result_img, cv2.imread("src/thumbs/" + thumb_list[thumb_cnt])[:height_upper - 5, :]))
     pre_word = cur_word
     pre_img = cur_img
     pre_bin = cur_bin
